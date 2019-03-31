20137dd28364c7a4bcf0f258117ea8a061b5f553
pheldox <felixaryee4@gmail.com>
2019-03-31 21:33:31 +0000

    Initial commit

---
+++ app/src/main/java/com/pheldox/blind/MainActivity.java
@@ -0,0 +1,149 @@
+package com.pheldox.blind;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class  MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private static final String TAG = "MainActivity";
+    Button start;
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak, microphone;
+    TextToSpeech tts;
+    String dest;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // start = (Button) findViewById(R.id.start_btn);
+        microphone = (ImageButton)findViewById(R.id.micro);
+
+        microphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+
+            }
+        });
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language is not supported");
+                    }
+                    speak("Hello Please press screen and say something");
+
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
+
+    }
+
+    private void speak(String text){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+
+        }else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak up");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Speech not Supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    dest = result.get(0);
+                    tts = new TextToSpeech(this,this);
+                }
+                break;
+            }
+
+        }
+    }
+
+    void startNavigation()
+    {
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + dest + "&mode=w");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+       // MainActivity.this.startService(new Intent(MainActivity.this,touchservice.class));
+        startActivity(mapIntent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onInit(int status) {
+        {
+            tts.setLanguage(Locale.UK);
+            Log.i(TAG, "onInit:  TTS Initialised");
+            int answer = tts.speak("You mentioned " + dest + " as your destination  Starting Navigation please",TextToSpeech.QUEUE_ADD,null,"Destination");
+            Log.i(TAG, "onActivityResult: Speech Result: " + answer);
+            tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String utteranceId) {
+
+                }
+
+                @Override
+                public void onDone(String utteranceId) {
+                    tts.stop();
+                    tts.shutdown();
+                    startNavigation();
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+
+                }
+            });
+        }
+    }
+
+}
